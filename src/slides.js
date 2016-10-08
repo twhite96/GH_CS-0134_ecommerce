ca971e4a507d5ca4f972efe88ae0b6bb8f0487aa
Tiffany White <tiffanywhitedev412@gmail.com>
2016-10-07 20:27:07 -0400

    Adds slick.js slider and unoptimized images. Will optimize soon

---
+++ src/slides.js
@@ -0,0 +1,40 @@
+
+$('.slide').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  accessibility: true,
+  arrows: true,
+  cssEase: 'ease',
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
